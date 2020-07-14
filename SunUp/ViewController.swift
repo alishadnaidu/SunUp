e657be079b9c69e5a0c9a9f92fe4a9577b9f1209
alisha naidu <alishadnaidu@gmail.com>
2020-07-14 14:26:22 -0700

    test

---
+++ SunUp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SunUp
+//
+//  Created by Alisha Naidu on 7/14/20.
+//  Copyright © 2020 Alisha Naidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var test = 0
+    }
+
+
+}
+
